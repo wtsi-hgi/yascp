e07d731494b77e896f84a3d3e5e54765ea3ecb05
Hannes Ponstingl <hp3@sanger.ac.uk>
2022-05-31 18:02:04 +0100

    Fixed identical rows output with the matrix conversion of bcftools gtcheck output needed for plots.

--- bin/gtcheck_assign.py
+++ bin/gtcheck_assign.py
@@ -2,8 +2,11 @@
 
 ## pick out the closes matching pair from output by bcftools gtcheck
 
+DEBUG = False
+
 import sys
 import statistics
+import copy
 
 SMVAL = 1.0E-9
 
@@ -12,7 +15,8 @@ def parse_gtcheck(infh):
     for lin in infh:
         if lin[:2] == 'DC':
             fld = lin.strip().split('\t')
-            print(fld)
+            if DEBUG:
+                print(fld)
             if len(fld) > 3 and 'DC' == fld[0].strip():
                 donor_query = fld[1].strip()
                 donor_ref = fld[2].strip()
@@ -24,57 +28,119 @@ def parse_gtcheck(infh):
                     gtchkd[donor_query] = [tup]
     return gtchkd
 
+def write_score_matrix(oufh, gtchkd):
+    # get names of reference panel
+    mtx = []
+    donor_ids = tuple(gtchkd.keys())
+    gt_ids = set()
+    # print(gtchkd.values())
+    for d in donor_ids:
+        for gtid, score in gtchkd[d]:
+            gt_ids.add(gtid)
+    genotype_ids = tuple(gt_ids)
+    n_donors = len(donor_ids)
+    n_genotypes = len(genotype_ids)
+    row_init = [float(0)]*n_genotypes
+    for i in range(n_donors):
+        donor_id = donor_ids[i]
+        mtx.append([0 for k in range(n_genotypes)])
+        for (gtid, score) in gtchkd[donor_id]:
+            try:
+                j = genotype_ids.index(gtid)
+            except ValueError:
+                sys.exit("ERROR: genotype id '{:s}' not found.".format(gtid))
+            mtx[i][j] = score
+    for d in donor_ids:
+        oufh.write(",{:s}".format(d))
+    oufh.write("\n")
+    for j in range(n_genotypes):
+        oufh.write("{:s}".format(genotype_ids[j]))
+        for i in range(n_donors):
+            oufh.write(",{:.1f}".format(mtx[i][j]))
+        oufh.write("\n")
+    return mtx, donor_ids, genotype_ids
+
+
 def find_matches(gtchkd):
     rsltd = {}
     for k in gtchkd:
         gtchkd[k].sort(key = lambda a: a[1])
-        print(gtchkd[k])
+        if DEBUG:
+            print(gtchkd[k])
         donor0, score0 = gtchkd[k][0]
-        dm = 0
-        d1 = 0
+        score1 = 0
+        score_n = 0
+        n = 0
+        d0 = float(0)
+        d1 = float(0)
+        sd = float(0)
+        m = float(0)
         sd = SMVAL
         if len(gtchkd[k]) > 1:
             donor1, score1 = gtchkd[k][1]
-            d1 = score1 - score0
+            dd1 = score1 - score0
             scores = []
             for i in range(1, len(gtchkd[k])):
                 scores.append(gtchkd[k][i][1])
             m = statistics.mean(scores)
-            dm = m - score0
+            d0 = m - score0
+            d1 = m - score1
             sd = statistics.stdev(scores)
             if sd < SMVAL:
                 sd = SMVAL
-            if dm < 1.5*sd:
-                print(scores, m, sd, score0, dm)
-                sys.stderr.write("ERROR: assignment possibly ambiguous.\n")
-
-        rsltd[k] = (donor0, dm/sd, d1/sd)
+            if d0 < 1.5*sd:
+                print("WARNING: ambiguous assignment", donor0, donor1, score0, score1, m, sd, d0, d1)
+                sys.stderr.write("ERROR: {:s} assignment ambiguous.\n".format(donor0))
+            score_n = scores[-1]
+            n = len(scores)
+        z0 = d0/sd
+        z1 = d1/sd
+        rsltd[k] = (donor0, score0, score1, score_n, n, m, sd, z0, z1, dd1/sd)
     return rsltd
 
 if __name__ == '__main__':
     narg = len(sys.argv)
     if narg != 3:
-        sys.exit("usage: {:s} <input file> <output file>".format(sys.argv[0]))
+        sys.exit("usage: {:s} <input file> <ouput file prefix>".format(sys.argv[0]))
 
     fnin = sys.argv[1]
-    fnout = sys.argv[2]
+    oufnprfx = sys.argv[2]
+
+    fnmtx = oufnprfx + '_gtcheck_score_table.csv'
+    fnout = oufnprfx + '_gtcheck_donor_assignments.csv'
 
     if fnin == '-':
         infh = sys.stdin
     else:
         infh = open(fnin, 'r')
-    oufh = open(fnout, 'w')
-    oufh.write("donor_query,donor_gt,z_score,z1_score\n")
     gtchkd = parse_gtcheck(infh)
-    print(gtchkd)
+    infh.close()
+    if DEBUG:
+        print(gtchkd)
+
+    oufh_mtx = open(fnmtx, 'w')
+    write_score_matrix(oufh_mtx, gtchkd)
+    oufh_mtx.close()
+
+    oufh = open(fnout, 'w')
+    oufh.write("donor_query,donor_gt,score0,score1,score_n,n,mean,sd,z0,z1\n")
+
     rsltd = find_matches(gtchkd)
-    print(rsltd)
+    if DEBUG:
+        print(rsltd)
     sys.stdout.write("\nAssignments:\n")
     for k in rsltd:
-        t = rsltd[k]
-        sys.stdout.write("{:s} -> {:s} z = {:f} d1 = {:f}\n".format(k, t[0], t[1], t[2]))
-        oufh.write("{:s},{:s},{:.1f},{:.1f}\n".format(k, t[0], t[1], t[2]))
+        (donorid, score0, score1, score_n, n, m, sd, z0, z1, zz1) = rsltd[k]
+        if z0 < SMVAL:
+            donorid = 'UNASSIGNED'
+        sys.stdout.write(
+            "{:s} -> {:s} score0 = {:.1f}, score1 = {:.1f}, score_last = {:.1f}, n_samples = {:d}, "
+            "mean = {:.1f}, sd = {:.1f}, z0 = {:.1f}, z1 = {:.1f}, zz1 = {:.1f}\n"
+            .format(k, donorid, score0, score1, score_n, n, m, sd, z0, z1, zz1)
+            )
+        oufh.write("{:s},{:s},{:.1f},{:.1f},{:.1f},{:d},{:.1f},{:.1f},{:.1f},{:.1f}\n"
+            .format(k, donorid, score0, score1, score_n, n, m, sd, z0, z1)
+            )
     oufh.close()
-    infh.close()
 
     sys.exit(0)
