844e37e47ec94613089821cf8a70c0b2b6a8be17
Matiss Ozols <mo11@sanger.ac.uk>
2023-08-18 13:42:10 +0100

    small change

--- assets/deploy_scripts/bsub.sh
+++ assets/deploy_scripts/bsub.sh
@@ -6,3 +6,4 @@ export RUN_ID="${PWD##*/}"
 sample="$RUN_ID.yascp"
 echo "Submitting yascp (https://github.com/wtsi-hgi/yascp) with input file $INPUT_FILE"
 bsub -R'select[mem>4000] rusage[mem=4000]' -J $sample -n 1 -M 4000 -o $sample.o -e $sample.e -q long bash /software/hgi/pipelines/yascp/assets/deploy_scripts/nohup_start_nextflow_lsf.sh $INPUT_FILE
+echo "Submitted job can be killed with: bkill -J $sample"
--- assets/deploy_scripts/bsub_test.sh
+++ assets/deploy_scripts/bsub_test.sh
@@ -6,3 +6,4 @@ export RUN_ID="${PWD##*/}"
 sample="$RUN_ID.yascp"
 echo "Submitting yascp (https://github.com/wtsi-hgi/yascp) in test mode withsample OneK1k dataset"
 bsub -R'select[mem>4000] rusage[mem=4000]' -J yascp_test -n 1 -M 4000 -o yascp_test.o -e yascp_test.e -q long bash /software/hgi/pipelines/yascp/assets/deploy_scripts/nohup_start_nextflow_lsf_test.sh
+echo "Submitted job can be killed with: bkill -J yascp_test"
