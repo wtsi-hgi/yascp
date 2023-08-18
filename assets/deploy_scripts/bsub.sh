6b635f3b6584b4587c0918a50ba1be685aa0f43d
Matiss Ozols <mo11@sanger.ac.uk>
2023-08-18 11:47:57 +0100

    test profiles added

--- assets/deploy_scripts/bsub.sh
+++ assets/deploy_scripts/bsub.sh
@@ -4,5 +4,5 @@ parentdir="$(dirname "$CWD1")"
 INPUT_FILE=$1
 export RUN_ID="${PWD##*/}"
 sample="$RUN_ID.yascp"
-echo "submitting yascp with input file $INPUT_FILE"
-bsub -R'select[mem>4000] rusage[mem=4000]' -G hgi -J $sample -n 1 -M 4000 -o $sample.o -e $sample.e -q long bash /software/hgi/pipelines/yascp/assets/deploy_scripts/nohup_start_nextflow_lsf.sh $INPUT_FILE
+echo "Submitting yascp (https://github.com/wtsi-hgi/yascp) with input file $INPUT_FILE"
+bsub -R'select[mem>4000] rusage[mem=4000]' -J $sample -n 1 -M 4000 -o $sample.o -e $sample.e -q long bash /software/hgi/pipelines/yascp/assets/deploy_scripts/nohup_start_nextflow_lsf.sh $INPUT_FILE
---
+++ assets/deploy_scripts/bsub_test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+CWD1="$PWD"
+parentdir="$(dirname "$CWD1")"
+INPUT_FILE=$1
+export RUN_ID="${PWD##*/}"
+sample="$RUN_ID.yascp"
+echo "Submitting yascp (https://github.com/wtsi-hgi/yascp) in test mode withsample OneK1k dataset"
+bsub -R'select[mem>4000] rusage[mem=4000]' -J yascp_test -n 1 -M 4000 -o yascp_test.o -e yascp_test.e -q long bash /software/hgi/pipelines/yascp/assets/deploy_scripts/nohup_start_nextflow_lsf_test.sh
