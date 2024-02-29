bc0a75190f3880ff07412299948757e001d606f7
maxozo <mo11@sanger.ac.uk>
2024-02-29 15:26:13 +0000

    makin sure v1.3 is checked out

--- assets/deploy_scripts/bsub_test_recluster.sh
+++ assets/deploy_scripts/bsub_test_recluster.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 CWD1="$PWD"
 parentdir="$(dirname "$CWD1")"
-INPUT_FILE=$1
+INPUT_FILE="$@"
 export RUN_ID="${PWD##*/}"
 
 # export SINGULARITY_CACHEDIR='/software/hgi/containers/yascp'
@@ -24,6 +24,6 @@ if ["$varname" = ''];
 fi
 
 sample="$RUN_ID.yascp"
-echo -e "\nSubmitting yascp (https://github.com/wtsi-hgi/yascp) in JUST_RECLUSTER mode with input file $INPUT_FILE"
-bsub -R'select[mem>4000] rusage[mem=4000]' -J yascp_cluster -n 1 -M 4000 -o yascp_cluster.o -e yascp_cluster.e -q normal bash /software/hgi/pipelines/yascp_versions/yascp_v1.3/assets/deploy_scripts/nohup_start_nextflow_lsf_recluster.sh  $INPUT_FILE
+echo -e "\nSubmitting yascp (https://github.com/wtsi-hgi/yascp) in CLUSTER mode running integration and clustering with input file $INPUT_FILE"
+bsub -R'select[mem>4000] rusage[mem=4000]' -J yascp_cluster -n 1 -M 4000 -o yascp_cluster.o -e yascp_cluster.e -q normal bash $SCRIPT_DIR/../../assets/deploy_scripts/nohup_start_nextflow_lsf_recluster.sh  $INPUT_FILE
 echo "Submitted job can be killed with: bkill -J yascp_cluster"
