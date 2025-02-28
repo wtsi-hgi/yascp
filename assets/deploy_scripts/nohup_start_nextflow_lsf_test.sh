90b058c02852bee04767d777f5be9f94d1e9560d
Matiss Ozols <mo11@sanger.ac.uk>
2025-02-28 13:40:30 +0000

    all exposed

--- assets/deploy_scripts/nohup_start_nextflow_lsf_test.sh
+++ assets/deploy_scripts/nohup_start_nextflow_lsf_test.sh
@@ -10,24 +10,19 @@ rm -f *.log
 rm -f nextflow.nohup.PID.txt 
 
 # start Nextflow in background:
-# export NXF_ANSI_LOG=false
-export NXF_EXECUTOR_LOG=true
-export NXF_LOG_LEVEL=TRACE
-export NXF_CLI_LOG=true
-export NXF_CONSOLE_DUMP=true
-export NXF_DEBUG=5
+export NXF_OPTS="-Xms5G -Xmx5G"
 
 CWD1="$PWD"
 parentdir="$(dirname "$CWD1")"
 # export RUN_ID="${parentdir##*/}"
 export RUN_ID="${PWD##*/}"
+
 mkdir $PWD/work || echo 'exists'
 mkdir $PWD/work/tmp || echo 'exists'
-echo $RUN_ID | unbuffer nextflow run $SCRIPT_DIR/../.. -profile sanger,test $INPUT_FILE --nf_ci_loc $PWD -resume  2>&1 | \
+echo $RUN_ID | nextflow run $SCRIPT_DIR/../.. -profile sanger,test $INPUT_FILE --nf_ci_loc $PWD -resume -with-trace  2>&1 | \
     sed -r "s/\x1b\[[0-9;]*m//g" | \
     ts '[%Y-%m-%d %H:%M:%S]' | \
-    grep -v '^\[[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\]$'  > nextflow.nohup.log &
-
+    grep -v '^\[[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\]$' > nextflow.nohup.log &
 # get process PID 
 sleep 1 && export PID=$(pgrep -f "\\-\\-nf_ci_loc $RUN_DIR")
 echo $PID > nextflow.nohup.PID.txt
